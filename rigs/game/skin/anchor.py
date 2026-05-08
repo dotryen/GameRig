5b28de6330306b65131ea159aa4c6a25e704224e
dotRYEN <work@dotryen.me>
2026-05-08 11:18:50 -0400

    fix non-deforming skin.anchor

--- rigs/game/skin/anchor.py
+++ rigs/game/skin/anchor.py
@@ -7,7 +7,6 @@ from rigify.base_rig import stage
 
 class Rig(BaseSkinChainRigWithRotationOption, anchor):
 
-    @stage.parent_bones
     def parent_deform_chain(self):
         if self.make_deform:
             self.set_bone_parent(self.bones.deform, self.rig_parent_bone)
@@ -15,9 +14,9 @@ class Rig(BaseSkinChainRigWithRotationOption, anchor):
     
     @stage.rig_bones
     def rig_deform_bone(self):
-        self.make_constraint(self.bones.deform, 'COPY_LOCATION', self.bones.org)
-        self.make_constraint(self.bones.deform, 'COPY_ROTATION', self.bones.org)
-
+        if self.make_deform:
+            self.make_constraint(self.bones.deform, 'COPY_LOCATION', self.bones.org)
+            self.make_constraint(self.bones.deform, 'COPY_ROTATION', self.bones.org)
 
 def create_sample(obj):
     from ..basic.super_copy import create_sample as inner
